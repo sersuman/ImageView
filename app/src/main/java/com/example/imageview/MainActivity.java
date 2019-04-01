deb51a743d46ac9c2be46e7eeaad8e98e1fa6c8a
Suman <sumanshrestha670@gmail.com>
2019-04-01 11:01:15 +0545

    click listener event

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private RadioButton rdoRajeshhamal, rdoBhuvanKc, rdoDayahang;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rdoBhuvanKc = findViewById(R.id.rdoBhuvanKc);
+        rdoDayahang = findViewById(R.id.rdoDayahang);
+        rdoRajeshhamal = findViewById(R.id.rdoRajeshHamal);
+
+        rdoDayahang.setOnClickListener(this);
+        rdoRajeshhamal.setOnClickListener(this);
+        rdoBhuvanKc.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.rdoBhuvanKc:
+                break;
+            case R.id.rdoDayahang:
+                break;
+            case R.id.rdoRajeshHamal:
+                break;
+        }
     }
 }
