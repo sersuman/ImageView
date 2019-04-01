4e0e27e9dcb41ced3d6af6261ce8d9c694e0f2db
Suman <sumanshrestha670@gmail.com>
2019-04-01 11:34:41 +0545

    Display image while clicking radio button

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.imageview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private RadioButton rdoRajeshhamal, rdoBhuvanKc, rdoDayahang;
+    private ImageView ivImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rdoBhuvanKc = findViewById(R.id.rdoBhuvanKc);
         rdoDayahang = findViewById(R.id.rdoDayahang);
         rdoRajeshhamal = findViewById(R.id.rdoRajeshHamal);
+        ivImage = findViewById(R.id.ivImage);
 
         rdoDayahang.setOnClickListener(this);
         rdoRajeshhamal.setOnClickListener(this);
@@ -26,13 +29,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.rdoBhuvanKc:
-                Toast.makeText(MainActivity.this, "Bhuvan KC", Toast.LENGTH_LONG).show();
+                ivImage.setImageResource(R.drawable.bhuvan);
+                Toast toast = Toast.makeText(MainActivity.this, "Bhuvan KC", Toast.LENGTH_LONG);
+                toast.show();
                 break;
             case R.id.rdoDayahang:
-                Toast.makeText(MainActivity.this, "Dayahang Rai", Toast.LENGTH_LONG).show();
+                ivImage.setImageResource(R.drawable.dayahang);
+                Toast toast1 = Toast.makeText(MainActivity.this, "Dayahang Rai", Toast.LENGTH_LONG);
+                toast1.show();
                 break;
             case R.id.rdoRajeshHamal:
-                Toast.makeText(MainActivity.this, "Rajesh Hamal", Toast.LENGTH_LONG).show();
+                ivImage.setImageResource(R.drawable.rajeshhamal);
+                Toast toast2 = Toast.makeText(MainActivity.this, "Rajesh Hamal", Toast.LENGTH_LONG);
+                toast2.show();
                 break;
         }
     }
